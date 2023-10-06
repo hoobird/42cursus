80e62afbf73504978e03b884c50f5fe5fd9228cd
Hubert Shing Yi Lim <hulim@c2r6s2.42singapore.sg>
2023-10-06 16:32:28 +0800

    asd

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -14,8 +14,66 @@
 
 char	*get_next_line(int fd)
 {
-	char	*currline
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	static t_list	*list = NULL;
+	char			*nextline;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &nextline, 0) < 0)
 		return (NULL);
+	currlinetolist(&list, fd);
+	if (list == NULL)
+		return (NULL);
+	nextline = linetostr(list);
+	resetlist(list);
+	return (nextline);
+}
+
+void	currlinetolist(t_list *list, int fd)
+{
+	char	*temp;
+	int	count;
+	t_list	*templist;
+
+	while (hasnewline(list) == 0)
+	{
+		temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!temp)
+			return ;
+		count = read(fd, &temp, BUFFER_SIZE);
+		if (count == 0)
+		{
+			free(temp);
+			return ;
+		}
+		temp[count] = 0;
+		templist = ft_lstnew(temp);
+		if (!templist)
+			return ;
+		ft_lstadd_back(templist);
+	}
+}
+
+int	hasnewline(t_list *list)
+{
+	char	*str;
 	
+	str = list->content;
+	while (*str)
+	{
+		if (*str == '\n')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+char 	*linetostr(t_list *list)
+{
+	char	*output;
+
+	
+}
+
+void	resetlist()
+{
+
 }
