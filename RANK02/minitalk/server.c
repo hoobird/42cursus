6d03200a8a0b14199dd8db97033c82e0266ec52f
hoobird99 <hubertlimshingyi@gmail.com>
2024-04-06 23:24:30 +0800

    hello

--- RANK02/minitalk/server.c
+++ RANK02/minitalk/server.c
@@ -6,9 +6,19 @@
 /*   By: hulim <hulim@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 16:56:05 by hulim             #+#    #+#             */
-/*   Updated: 2024/04/06 17:46:00 by hulim            ###   ########.fr       */
+/*   Updated: 2024/04/06 18:29:54 by hulim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "ft_printf/ft_printf.h"
 
+int main(void)
+{
+    int pid;
+
+    pid = getpid();
+    ft_printf("Server PID: %d\n", pid);
+    
+    return (0);
+}
