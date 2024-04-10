ab42e843f6b5bc11e524a349a2d1417ac876aa42
Hubert <hubertlimshingyi@gmail.com>
2024-04-10 19:49:19 +0800

    minitalk done with errors

--- RANK02/minitalk/client.c
+++ RANK02/minitalk/client.c
@@ -6,24 +6,48 @@
 /*   By: hulim <hulim@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 01:27:00 by hulim             #+#    #+#             */
-/*   Updated: 2024/04/08 02:41:55 by hulim            ###   ########.fr       */
+/*   Updated: 2024/04/10 18:52:11 by hulim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int	sendmsginbits(int pid, char *str);
+
 int	main(int argc, char **argv)
 {
-	int		pid;
-	char	*message;
-
 	if (argc != 3)
 	{
 		ft_printf("Error: Client takes 2 parameters\n");
 		ft_printf("Server PID and String to send\neg: ./client (pid) (msg)\n");
 		return (1);
 	}
-	pid = ft_atoi(argv[1]);
-	message = argv[2];
-	ft_printf("%d : %s\n", pid, message);
+	if (sendmsginbits(ft_atoi(argv[1]), argv[2]) == -1)
+		ft_printf("Error: Unable to sned signal to specified PID\n");
+	return (0);
+}
+
+int	sendmsginbits(int pid, char *str)
+{
+	size_t	bitcount;
+	int		outcome;
+
+	while (*str)
+	{
+		bitcount = 0;
+		while (bitcount < 8)
+		{
+			if (1 & (*str >> bitcount))
+				outcome = kill(pid, SIGUSR1);
+			else
+				outcome = kill(pid, SIGUSR2);
+			if (outcome == -1)
+				return (outcome);
+			bitcount++;
+			usleep(10000);
+		}
+		bitcount = 0;
+		str++;
+	}
+	return (0);
 }
--- RANK02/minitalk/server.c
+++ RANK02/minitalk/server.c
@@ -6,21 +6,52 @@
 /*   By: hulim <hulim@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 16:56:05 by hulim             #+#    #+#             */
-/*   Updated: 2024/04/08 03:57:58 by hulim            ###   ########.fr       */
+/*   Updated: 2024/04/10 18:40:32 by hulim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(void)
+void	bittochar(int sigusr);
+
+int	main(int argc, char **argv)
 {
 	int	pid;
 
+	(void) argv;
+	if (argc != 1)
+	{
+		ft_printf("Error: Server doesn't take parameters\n");
+		ft_printf("eg: ./server\n");
+		return (1);
+	}
 	pid = getpid();
 	ft_printf("Server PID: %d\n", pid);
 	while (1)
 	{
+		signal(SIGUSR1, bittochar);
+		signal(SIGUSR2, bittochar);
 		pause();
 	}
 	return (0);
+}
+
+void	bittochar(int sigusr)
+{
+	static char		letter;
+	static size_t	bitcount;
+	unsigned char	mask;
+
+	if (sigusr == SIGUSR1)
+	{
+		mask = 1 << bitcount;
+		letter = letter | mask;
+	}
+	bitcount++;
+	if (bitcount == 8)
+	{
+		ft_printf("%c", letter);
+		bitcount = 0;
+		letter = 0;
+	}
 }
